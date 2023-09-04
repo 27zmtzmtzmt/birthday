bcf61d298fe1123c29504edb874ce1b616ac09f3
27zmtzmtzmt <143988901+27zmtzmtzmt@users.noreply.github.com>
2023-09-04 00:09:29 -0700

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        //
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
@@ -43,5 +43,21 @@ var config = {
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
         story: "A MESSAGE FOR YOU",
-    }
-};
+ texts:[
+        "today is ur birthday",
+        "happy birthday to you",
+        "you were born when you were born",
+        "you born where you born",
+        "you grew up when you grew up",
+        "you ate the birthday cakes as many as how many years you lived(except when you can't eat yet",
+        "when today ends, your age grew up by one",
+        "the candles might be your age, or as many as your age",
+        "or maybe not",
+        "happy birthday"
+        "here's your digital birthday cake:"
+        "( o˘◡˘o) ┌iii┐"
+        "blow out the candles!"
+        "( o˘◡˘o) ┌lll┐"
+        "good job"
+        "have a great birthday!"
+        ],
