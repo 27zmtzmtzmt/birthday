919f1def72803de0157607c47ff8a14ac343cd7c
27zmtzmtzmt <143988901+27zmtzmtzmt@users.noreply.github.com>
2023-09-03 23:24:36 -0700

    shengri

--- config.js
+++ config.js
@@ -3,17 +3,22 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "today is ur birthday",
+        "happy birthday to you",
+        "you were born when you were born",
+        "you born where you born",
+        "you grew up when you grew up",
+        "you ate the birthday cakes as many as how many years you lived(except when you can't eat yet",
+        "when today ends, your age grew up by one",
+        "the candles might be your age, or as many as your age",
+        "or maybe not",
+        "happy birthday"
+        "here's your digital birthday cake:"
+        "( o˘◡˘o) ┌iii┐"
+        "blow out the candles!"
+        "( o˘◡˘o) ┌111┐"
+        "good job"
+        "have a great birthday!"
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
